7a6554d2b74adb0bde358698918aa283f7086689
Kevin Stähler <phrenchen@gmail.com>
2018-08-10 22:51:42 +0200

    added service worker for offline functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,17 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// SERVICE WORKER
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw_pagecache.js').then(function(registration) {
+      // Registration was successful
+      //console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      //console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
